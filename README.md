ce1bfdb4e2ff2a203f0f7c37cd7a98fb01c78c4b
Reborn233 <499289113@qq.com>
2017-06-05 18:42:28 +0800

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ###记录各种学习过程中的特效动画
+
+[链接](http://reborn233.top/effects.github.io/)
---
+++ js/sdm.js
@@ -0,0 +1,11 @@
+var getId = function(str){
+	return document.getElementById(str);
+};
+
+var getClass = function(str){
+	return document.getElementsByClassName(str);
+};
+
+var getTagName = function(str){
+	return document.getElementsByTagName(str);
+};
